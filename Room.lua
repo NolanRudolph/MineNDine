c360d3a6913853c50222b366290146874184a1e6
NolanRudolph <nolanrudolph1@gmail.com>
2018-09-10 03:43:57 -0700

    Implemented Phong Shading Algorithm!

--- Room.lua
+++ Room.lua
@@ -4,13 +4,12 @@ Tile = require 'Tile'
 nextRoomID = 0
 TILE_PX_SIZEX = 32
 TILE_PX_SIZEY = 12
-WIDTH = love.graphics.getWidth()
-HEIGHT = love.graphics.getHeight()
 X_HALF = WIDTH/2
 X_ADJUST = (WIDTH/TILE_PX_SIZEX)/2
 PATH_X = {HEIGHT/8 + 3 * (HEIGHT/8), HEIGHT/8 + 4 * (HEIGHT/8)}
 Y_ADJUST = (HEIGHT/TILE_PX_SIZEY)/2
 TILE_OFFSET = 16
+
 
 function Room:new(room, IDX, IDY, index)
     room = room or {}
@@ -32,56 +31,56 @@ function Room:initTiles()
         local tileX = 0
         for tileX = 0, WIDTH/TILE_PX_SIZEX do
             local isOffset = offset % 2
-            local x
-            if isOffset == 0 then
-                x = tileX * TILE_PX_SIZEX
-            elseif isOffset == 1 then
-                x = tileX * TILE_PX_SIZEX - TILE_OFFSET
-            end
             local y = tileY * TILE_PX_SIZEY
             local image
             local actualX = (tileX - X_ADJUST) * TILE_PX_SIZEX
             local hypot = math.sqrt(math.pow(actualX, 2) + math.pow(actualY, 2))
             local deg = (180/math.pi) * math.acos(actualX/hypot)
+            local newX
+            if isOffset == 0 then
+                newX = tileX * TILE_PX_SIZEX
+            elseif isOffset == 1 then
+                newX = tileX * TILE_PX_SIZEX - TILE_OFFSET
+            end
             if hypot <= 350 then
                 if hypot <= 150 then
-                    table.insert(self.tiles, Tile:new(x, y, 'middle'))
+                    table.insert(self.tiles, Tile:new(newX, y, 'middle'))
                 elseif deg >= 25 and deg <= 65 then
                     if tileY < Y_ADJUST then
-                        table.insert(self.tiles, Tile:new(x, y, 'topRight'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'topRight'))
                     else
-                        table.insert(self.tiles, Tile:new(x, y, 'botRight'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'botRight'))
                     end
                 elseif deg >= 65 and deg <= 115 then
                     if tileY < Y_ADJUST then
-                        table.insert(self.tiles, Tile:new(x, y, 'topMid'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'topMid'))
                     else
-                        table.insert(self.tiles, Tile:new(x, y, 'botMid'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'botMid'))
                     end
                 elseif deg >= 115 and deg <= 155 then
                     if tileY < Y_ADJUST then
-                        table.insert(self.tiles, Tile:new(x, y, 'topLeft'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'topLeft'))
                     else
-                        table.insert(self.tiles, Tile:new(x, y, 'botLeft'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'botLeft'))
                     end
                 else
                     if tileX < X_ADJUST then
-                        table.insert(self.tiles, Tile:new(x, y, 'leftMid'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'leftMid'))
                     else
-                        table.insert(self.tiles, Tile:new(x, y, 'rightMid'))
+                        table.insert(self.tiles, Tile:new(newX, y, 'rightMid'))
                     end
                 end
             elseif tileX >= 10 and tileX <= 16 then
                 if tileX < X_ADJUST then
-                    table.insert(self.tiles, Tile:new(x, y, 'topMid'))
+                    table.insert(self.tiles, Tile:new(newX, y, 'topMid'))
                 else
-                    table.insert(self.tiles, Tile:new(x, y, 'botMid'))
+                    table.insert(self.tiles, Tile:new(newX, y, 'botMid'))
                 end
             elseif tileY >= 25 and tileY <= 39 then
                 if tileX < Y_ADJUST then
-                    table.insert(self.tiles, Tile:new(x, y, 'leftMid'))
+                    table.insert(self.tiles, Tile:new(newX, y, 'leftMid'))
                 else
-                    table.insert(self.tiles, Tile:new(x, y, 'rightMid'))
+                    table.insert(self.tiles, Tile:new(newX, y, 'rightMid'))
                 end
             end
         end
--- main.lua
+++ main.lua
@@ -1,20 +1,68 @@
 -- main.lua
 
+WIDTH = love.graphics.getWidth()
+HEIGHT = love.graphics.getHeight()
+
 conf = require 'conf'
 troubleShoot = require 'troubleShoot'
 utils = require 'utils'
 Hero = require 'Hero'
 World = require 'World'
 Room = require 'Room'
-METER_IN_PX = 64
-TILE_PX_SIZEX = 32
-TILE_PX_SIZEY = 12
-TILE_OFFSET = 16
-ROOM_TILE_SIZEX = 24
-ROOM_TILE_SIZEY = 62
 
+METER_IN_PX = 64
+
+local shader_code = [[
+
+/* PHONG SHADING ALROGITHM */
+
+#define NUM_LIGHTS 32
+
+struct Light {
+    vec2 position;
+    vec3 diffuse;
+    float power;
+};
+
+extern Light lights[NUM_LIGHTS];
+extern int num_lights;
+extern vec2 screen;
+
+const float constant = 1.0;
+const float linear = 0.09;
+const float quadratic = 0.032;
+
+
+vec4 effect(vec4 color, Image image, vec2 uvs, vec2 screen_coords)
+{
+    vec4 pixel = Texel(image, uvs);
+
+    vec2 norm_screen = screen_coords / screen;
+    vec3 diffuse = vec3(0);
+
+    for (int i = 0; i < num_lights; i++)
+    {
+        Light light = lights[i];
+        vec2 norm_pos = light.position / screen;
+
+        float distance = length(norm_pos - norm_screen) * light.power;
+        float attenuation = 1.0 / (constant + linear * distance * 
+                                   quadratic * (distance * distance));
+
+        diffuse += light.diffuse * attenuation;
+    }
+
+    diffuse = clamp(diffuse, 0.0, 1.0);
+
+    return pixel * vec4(diffuse, 1.0);
+}
+
+]]
+
+-- love.graphics.rectangle('fill', 400, 396, 32, 12)
 
 function love.load()
+    shader = love.graphics.newShader(shader_code)
     world = love.physics.newWorld(0, 0, true)  --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
     
     --[[ Loading Physics ]]--
@@ -95,20 +143,34 @@ function love.update(dt)  -- Updates Every dt (Delta Time)
         Hero.body:setY(-90)
 
     end
-
-end
-
-
-function love.keydown(key)
-
 end
 
 
 function love.draw()  -- Updates Every Frame
+    love.graphics.setShader(shader)
+
+    shader:send("screen", {
+        love.graphics.getWidth(),
+        love.graphics.getHeight()
+    })
+
+    shader:send("num_lights", 1)
+
+    shader:send("lights[0].position", {
+        WIDTH / 2, HEIGHT / 2
+    })
+
+    shader:send("lights[0].diffuse", {
+        1, 1, 1
+    })
+
+    shader:send("lights[0].power", 15)
+
     World.rooms[World.currentIndex]:renderTiles()
     love.graphics.setColor(255, 255, 255)
     love.graphics.print('This is Room {' .. World.currentRoom[1] .. ', ' .. World.currentRoom[2] .. '}', 15, 15)
     Hero:renderHero()
+    love.graphics.setShader()
 
     -- troubleShoot:render()  --< For Brick Grid
     
